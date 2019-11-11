5455123590f64fdc8d85dddacdb63b090be3370a
NaoIkeji <na3o.g.7@gmail.com>
2019-11-11 08:24:26 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,27 @@
 class ApplicationController < ActionController::Base
-
+  protect_from_forgery
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   	def after_sign_in_path_for(resource)
   		if params[:admin] == nil
-  			user_my_page_path(current_user.id)
+  			 user_my_page_path(current_user.id)
   		else
-  			admin_home_top_path
+  			 admin_home_top_path
   		end
   	end
 
   	def after_sign_out_path_for(resource)
-  		  root_path
+      if params[:admin]
+         admin_session_path
+      else
+         root_path
+      end
   	end
 
   	protected
   	def configure_permitted_parameters
     	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-    		user_params.permit(:last_name, :first_name, :last_name_ruby, :first_name_ruby, :nickname)
+    		user_params.permit(:last_name, :first_name, :last_name_ruby, :first_name_ruby, :nickname, :email, :password)
     	end
   	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,36 +2,46 @@ Rails.application.routes.draw do
 
   get '/' => 'home#top', as: 'root'
 
-namespace :user do
-  get '/users/:id/my_page' => 'users#my_page', as: 'my_page'
-  get '/users/:id/daily_meal' => 'users#daily_meal', as: 'daily_meal'
-  # get '/users/daily_meal/:id/date[0]/date[1]/date[2]' => 'users#my_page', as: 'daily_meal'
+  namespace :user do
+    get '/users/:id/my_page' => 'users#my_page', as: 'my_page'
+    get '/users/:id/daily_meal' => 'users#daily_meal', as: 'daily_meal'
+    get '/users/:id/daily_meal_balance' => 'users#daily_meal_balance', as: 'daily_meal_balance'
 
-  get '/users/:id/daily_meal_balance' => 'users#daily_meal_balance', as: 'daily_meal_balance'
+    get '/meals/meal_indication' =>'meals#meal_indication', as: 'meal_indication'
+    get '/user/contacts/contact_completed' => 'contacts#contact_complete', as: 'contact_complete'
 
+  	resources :meals
+  	resources :foods, only: [:create, :update, :edit, :destroy]
+  	resources :food_points, only: [:show, :new, :create, :update, :edit, :destroy]
+    resources :targets, only: [:create, :edit, :update, :destroy]
+    resources :contacts, only: [:new, :create]
 
-  get '/meals/meal_indication' =>'meals#meal_indication', as: 'meal_indication'
+    resources :posts do
+      resource :favorites, only: [:create, :destroy]
+      resources :comments, only: [:create, :edit, :update, :destroy]
+    end
 
-	resources :meals
-	resources :foods, only: [:create, :update, :edit, :destroy]
-	resources :food_points, only: [:show, :new, :create, :update, :edit, :destroy]
+    resources :users do
+      resource :relationships, only: [:create, :destroy]
+    end
+
   end
 
-namespace :admin do
-  get 'home/top' => 'home#top'
-	resources :foods, only: [:index, :new, :create, :update, :edit, :destroy]
-end
+  namespace :admin do
+    get 'home/top' => 'home#top'
+  	resources :foods, only: [:index, :new, :create, :update, :edit, :destroy]
+  end
 
-  devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-  }
+    devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+    }
 
-  devise_for :users, controllers: {
-  sessions:      'users/sessions',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations'
-  }
+    devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
